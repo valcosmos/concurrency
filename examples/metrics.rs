95e326065adf1bd9765d5ceb7ca0329da6db783a
Cupid Valentine <westerngod@126.com>
2024-10-16 23:54:16 +0800

    feat: try arc mutex

---
+++ examples/metrics.rs
@@ -0,0 +1,44 @@
+use std::{thread, time::Duration};
+
+use rand::Rng;
+use template::Metrics;
+
+const N: usize = 2;
+const M: usize = 4;
+
+fn main() {
+    let metrics = Metrics::new();
+
+    println!("{:?}", metrics.snapshot());
+
+    for idx in 0..N {
+        task_worker(idx, metrics.clone()); // Metric {data: Arc::clone(&metrics.data)}
+    }
+
+    for _ in 0..M {
+        request_worker(metrics.clone());
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(5));
+        println!("{:?}", metrics.snapshot());
+    }
+}
+
+fn task_worker(ids: usize, metrics: Metrics) {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+        // do long term stuff 做一些事情
+        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+        metrics.inc(format!("call.thread.worker.{}", ids)).unwrap();
+    });
+}
+
+fn request_worker(metrics: Metrics) {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+        let page = rng.gen_range(1..256);
+        metrics.inc(format!("req.page.{}", page)).unwrap();
+    });
+}
---
+++ src/metrics.rs
@@ -0,0 +1,42 @@
+use anyhow::{anyhow, Ok, Result};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+#[derive(Debug, Clone)]
+pub struct Metrics {
+    data: Arc<Mutex<HashMap<String, i64>>>,
+}
+
+#[allow(clippy::new_without_default)]
+impl Metrics {
+    pub fn new() -> Self {
+        Metrics {
+            data: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    pub fn inc(&self, key: impl Into<String>) -> Result<()> {
+        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let counter = data.entry(key.into()).or_insert(0);
+        *counter += 1;
+        Ok(())
+    }
+
+    pub fn dec(&self, key: impl Into<String>) -> Result<()> {
+        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let key = key.into();
+        let counter = data.entry(key).or_insert(0);
+        *counter -= 1;
+        Ok(())
+    }
+
+    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
+        Ok(self
+            .data
+            .lock()
+            .map_err(|e| anyhow!(e.to_string()))?
+            .clone())
+    }
+}
