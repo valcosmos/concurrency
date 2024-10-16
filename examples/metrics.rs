3f2ce8be4f12f1d634189f2cce97801148194b59
Cupid Valentine <westerngod@126.com>
2024-10-17 00:02:31 +0800

    refactor: remove unwrap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -1,22 +1,22 @@
-use std::{thread, time::Duration};
-
+use anyhow::Result;
 use rand::Rng;
+use std::{thread, time::Duration};
 use template::Metrics;
 
 const N: usize = 2;
 const M: usize = 4;
 
-fn main() {
+fn main() -> Result<()> {
     let metrics = Metrics::new();
 
     println!("{:?}", metrics.snapshot());
 
     for idx in 0..N {
-        task_worker(idx, metrics.clone()); // Metric {data: Arc::clone(&metrics.data)}
+        task_worker(idx, metrics.clone())?; // Metric {data: Arc::clone(&metrics.data)}
     }
 
     for _ in 0..M {
-        request_worker(metrics.clone());
+        request_worker(metrics.clone())?;
     }
 
     loop {
@@ -25,20 +25,30 @@ fn main() {
     }
 }
 
-fn task_worker(ids: usize, metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        // do long term stuff 做一些事情
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics.inc(format!("call.thread.worker.{}", ids)).unwrap();
+fn task_worker(ids: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            // do long term stuff 做一些事情
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", ids))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
 
-fn request_worker(metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page = rng.gen_range(1..256);
-        metrics.inc(format!("req.page.{}", page)).unwrap();
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..256);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
